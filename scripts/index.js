34274dde70b8cef5a65d4ce01ab89f5331cd38fd
Mayorov <vn.mayor@gmail.com>
2020-11-27 23:31:42 +0300

    feat: исправить замечания 1й итерации

--- scripts/index.js
+++ scripts/index.js
@@ -3,26 +3,33 @@ const titleProfileNode = document.querySelector('.profile__title');
 const subtitleProfileNode = document.querySelector('.profile__subtitle');
 const submitFormNode = document.querySelector('.popup__container');
 const closeButton = document.querySelector('.popup__close-button');
+const popupNode = document.querySelector('.popup');
+const popupNameNode = document.querySelector('.popup__input-el[name*=profile-name]');
+const popupJobNode = document.querySelector('.popup__input-el[name*=profile-job]');
+
+// функции
+function showPopup() {
+  popupNameNode.value = titleProfileNode.textContent;
+  popupJobNode.value = subtitleProfileNode.textContent;
+
+  popupNode.classList.add('popup_opened');
+}
+
+function setProfileData(evt) {
+
+  evt.preventDefault();
+
+  titleProfileNode.textContent = popupNameNode.value;
+  subtitleProfileNode.textContent = popupJobNode.value;
+
+  popupNode.classList.remove('popup_opened');
+}
+
+function closePopup() {
+  popupNode.classList.remove('popup_opened');
+}
 
 // слушатели
 editButton.addEventListener('click', showPopup);
 submitFormNode.addEventListener('submit', setProfileData);
-closeButton.addEventListener('click', () => document.querySelector('.popup').classList.remove('popup_opened'));
-
-// функции
-function showPopup () {
-  document.querySelector('.popup__name').value = titleProfileNode.textContent;
-  document.querySelector('.popup__job').value = subtitleProfileNode.textContent;
-
-  document.querySelector('.popup').classList.add('popup_opened');
-}
-
-function setProfileData (evt) {
-
-  evt.preventDefault();
-
-  titleProfileNode.textContent = document.querySelector('.popup__name').value;
-  subtitleProfileNode.textContent = document.querySelector('.popup__job').value;
-
-  document.querySelector('.popup').classList.remove('popup_opened');
-}
+closeButton.addEventListener('click', closePopup);
