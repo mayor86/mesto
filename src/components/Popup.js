ef2636ac6bcae948424b2d5f89344fa4564a45fd
Mayorov <vn.mayor@gmail.com>
2021-02-15 18:16:28 +0300

    feat:проектная работа 8, попытка 2

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -4,11 +4,7 @@ export default class Popup {
     this._closeButton = this._popup.querySelector('.popup__close-button');
     this._handleEscCloseBindings = this._handleEscClose.bind(this);
   }
-/*
-  document.addEventListener('click', (evt) => {
-    this._handleEscClose(evt);
-  });
-*/
+
   open() {
     this._popup.classList.add('popup_opened');
     document.addEventListener('keyup', this._handleEscCloseBindings);
--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,12 +1,9 @@
 import Popup from './Popup.js';
 
 export class PopupWithImage extends Popup {
-//  constructor(popupSelector, name, link) {
   constructor(popupSelector) {
     super(popupSelector);
     this._popup = document.querySelector(popupSelector);
-//    this._name = name;
-//    this._link = link;
     this._image = this._popup.querySelector('.popup-image__image');
     this._title = this._popup.querySelector('.popup-image__title');
   }
